beb271ba61e7a174caa9af7e83f29fbb377e3d46
Anton <simaginam@gmail.com>
2024-08-27 07:51:59 +0300

    refactor

---
+++ app/src/main/java/ru/apps/e1em/yandexadstemplate/YandexAdsAppOpen.java
@@ -0,0 +1,120 @@
+package ru.apps.e1em.yandexadstemplate;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ProcessLifecycleOwner;
+
+import com.yandex.mobile.ads.appopenad.AppOpenAd;
+import com.yandex.mobile.ads.appopenad.AppOpenAdEventListener;
+import com.yandex.mobile.ads.appopenad.AppOpenAdLoadListener;
+import com.yandex.mobile.ads.appopenad.AppOpenAdLoader;
+import com.yandex.mobile.ads.common.AdError;
+import com.yandex.mobile.ads.common.AdRequestConfiguration;
+import com.yandex.mobile.ads.common.AdRequestError;
+import com.yandex.mobile.ads.common.ImpressionData;
+
+public class YandexAdsAppOpen {
+    private Context mContext;
+    private Activity mActivity;
+    // Yandex Ads.
+    @Nullable
+    private AppOpenAd mAppOpenAd;
+    private final String AD_UNIT_ID  = "demo-banner-yandex";
+    private boolean isAdShowOnColdStart = false;
+
+    public YandexAdsAppOpen(Context context) {
+        mContext = context;
+        mActivity = (Activity)mContext;
+    }
+
+    public void init() {
+        loadAppOpenAd();
+        final DefaultProcessLifecycleObserver processLifecycleObserver = new DefaultProcessLifecycleObserver() {
+            @Override
+            public void onProcessCameForeground() { showAppOpenAd(); }
+        };
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(processLifecycleObserver);
+    }
+
+    private void loadAppOpenAd() {
+        final AppOpenAdLoader appOpenAdLoader = new AppOpenAdLoader(mContext);
+        AppOpenAdLoadListener appOpenAdLoadListener = new AppOpenAdLoadListener() {
+            @Override
+            public void onAdLoaded(@NonNull final AppOpenAd appOpenAd) {
+                // The ad was loaded successfully. Now you can show loaded ad.
+                mAppOpenAd = appOpenAd;
+                if(!isAdShowOnColdStart) {
+                    showAppOpenAd();
+                    isAdShowOnColdStart = true;
+                }
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdLoaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull final AdRequestError adRequestError) {
+                // Ad failed to load with AdRequestError.
+                // Attempting to load a new ad from the onAdFailedToLoad() method is strongly discouraged.
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdFailedToLoad");
+            }
+        };
+        appOpenAdLoader.setAdLoadListener(appOpenAdLoadListener);
+
+        final AdRequestConfiguration adRequestConfiguration = new AdRequestConfiguration.Builder(AD_UNIT_ID).build();
+        appOpenAdLoader.loadAd(adRequestConfiguration);
+    }
+
+    private void showAppOpenAd() {
+        AppOpenAdEventListener appOpenAdEventListener = new AppOpenAdEventListener() {
+            @Override
+            public void onAdShown() {
+                // Called when ad is shown.
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdShown");
+            }
+
+            @Override
+            public void onAdFailedToShow(@NonNull final AdError adError) {
+                // Called when ad failed to show.
+                clearAppOpenAd();
+                loadAppOpenAd();
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdFailedToShow");
+            }
+
+            @Override
+            public void onAdDismissed() {
+                // Called when ad is dismissed.
+                // Clean resources after dismiss and preload new ad.
+                clearAppOpenAd();
+                loadAppOpenAd();
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdDismissed");
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdClicked");
+            }
+
+            @Override
+            public void onAdImpression(@Nullable final ImpressionData impressionData) {
+                // Called when an impression is recorded for an ad.
+                Log.i("Yandex Ads",">>> Yandex Ads AppOpen onAdImpression" + impressionData);
+            }
+        };
+
+        if (mAppOpenAd != null) {
+            mAppOpenAd.setAdEventListener(appOpenAdEventListener);
+            mAppOpenAd.show(mActivity);
+        }
+    }
+
+    private void clearAppOpenAd() {
+        if (mAppOpenAd != null) {
+            mAppOpenAd.setAdEventListener(null);
+            mAppOpenAd = null;
+        }
+    }
+}
---
+++ app/src/main/java/ru/apps/e1em/yandexadstemplate/YandexAdsStickyBanner.java
@@ -0,0 +1,120 @@
+package ru.apps.e1em.yandexadstemplate;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.yandex.mobile.ads.banner.BannerAdEventListener;
+import com.yandex.mobile.ads.banner.BannerAdSize;
+import com.yandex.mobile.ads.banner.BannerAdView;
+import com.yandex.mobile.ads.common.AdRequest;
+import com.yandex.mobile.ads.common.AdRequestError;
+import com.yandex.mobile.ads.common.ImpressionData;
+
+import ru.apps.e1em.yandexadstemplate.databinding.ActivityMainBinding;
+
+public class YandexAdsStickyBanner {
+    private ActivityMainBinding mBinding;
+    private Context mContext;
+    private Activity mActivity;
+
+    // Yandex Ads.
+    @Nullable
+    private BannerAdView mBannerAd = null;
+    private final String AD_UNIT_ID  = "demo-banner-yandex";
+
+    public YandexAdsStickyBanner(ActivityMainBinding mainActivity, Context context)
+    {
+        mBinding = mainActivity;
+        mContext = context;
+        mActivity = (Activity)mContext;
+    }
+
+    public void init() {
+        mBinding.adContainerView.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        mBinding.adContainerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        mBannerAd = loadBannerAd(getAdSize());
+                    }
+                }
+        );
+    }
+
+    @NonNull
+    private BannerAdSize getAdSize() {
+        final DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        // Calculate the width of the ad, taking into account the padding in the ad container.
+        int adWidthPixels = mBinding.adContainerView.getWidth();
+        if (adWidthPixels == 0) {
+            // If the ad hasn't been laid out, default to the full screen width
+            adWidthPixels = displayMetrics.widthPixels;
+        }
+        final int adWidth = Math.round(adWidthPixels / displayMetrics.density);
+
+        return BannerAdSize.stickySize(mContext, adWidth);
+    }
+
+    @NonNull
+    private BannerAdView loadBannerAd(@NonNull final BannerAdSize adSize) {
+        final BannerAdView bannerAd = mBinding.adContainerView;
+        bannerAd.setAdSize(adSize);
+        bannerAd.setAdUnitId(AD_UNIT_ID );
+        bannerAd.setBannerAdEventListener(new BannerAdEventListener() {
+            @Override
+            public void onAdLoaded() {
+                // If this callback occurs after the activity is destroyed, you
+                // must call destroy and return or you may get a memory leak.
+                // Note `isDestroyed` is a method on Activity.
+                if (mActivity.isDestroyed() && mBannerAd != null) {
+                    mBannerAd.destroy();
+                }
+                Log.i("Yandex Ads",">>> Yandex Ads Banner onAdLoaded!");
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull final AdRequestError adRequestError) {
+                // Ad failed to load with AdRequestError.
+                // Attempting to load a new ad from the onAdFailedToLoad() method is strongly discouraged.
+                Log.i("Yandex Ads",">>> Yandex Ads Banner onAdFailedToLoad");
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.i("Yandex Ads",">>> Yandex Ads Banner onAdClicked");
+            }
+
+            @Override
+            public void onLeftApplication() {
+                // Called when user is about to leave application (e.g., to go to the browser), as a result of clicking on the ad.
+                Log.i("Yandex Ads",">>> Yandex Ads Banner onLeftApplication");
+            }
+
+            @Override
+            public void onReturnedToApplication() {
+                // Called when user returned to application after click.
+                Log.i("Yandex Ads",">>> Yandex Ads Banner onReturnedToApplication");
+            }
+
+            @Override
+            public void onImpression(@Nullable ImpressionData impressionData) {
+                // Called when an impression is recorded for an ad.
+                if (impressionData != null) {
+                    Log.i("Yandex Ads",">>> Yandex Ads Banner onImpression" + impressionData.getRawData());
+                }
+            }
+        });
+        final AdRequest adRequest = new AdRequest.Builder()
+                // Methods in the AdRequest.Builder class can be used here to specify individual options settings.
+                .build();
+        bannerAd.loadAd(adRequest);
+        return bannerAd;
+    }
+}
